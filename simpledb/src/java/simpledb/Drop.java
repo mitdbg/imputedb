e4147c6719707c0cfb9234c1c47a5e700f301c7b
Micah Smith <micahjsmith@gmail.com>
2016-11-18 13:59:42 -0500

    Move dropFields logic to Impute class.

--- simpledb/src/java/simpledb/Drop.java
+++ simpledb/src/java/simpledb/Drop.java
@@ -6,8 +6,6 @@ public class Drop extends Impute {
 
 	private static final long serialVersionUID = 1L;
 
-	private final Collection<String> dropFields;
-
 	/**
 	 * Deal with missing data on a set of column by simply dropping the rows
 	 * with missing data.
@@ -15,36 +13,7 @@ public class Drop extends Impute {
 	 * @param child
 	 */
 	public Drop(Collection<String> dropFields, DbIterator child) {
-		super(child);
-		if (validateDropFields(dropFields)){
-			this.dropFields = dropFields;
-		} else {
-			throw new RuntimeException("Could not validate dropFields.");
-		}
-	}
-
-	/**
-	 * Validate that each field in dropFields corresponds to a fieldName in
-	 * child's TupleDesc.
-	 * @param dropFields
-	 */
-	private boolean validateDropFields(Collection<String> dropFields) {
-		for (String field : dropFields){
-			if (field == null){
-				System.err.println("Could not validate null field.");
-				return false;
-			}
-
-			try{
-				td.fieldNameToIndex(field);
-			} catch (Exception e){
-				System.err.println("Could not validate field: "+field);
-				return false;
-			}
-		}
-		
-		return true;
-		
+		super(dropFields, child);
 	}
 
 	@Override
--- simpledb/src/java/simpledb/Impute.java
+++ simpledb/src/java/simpledb/Impute.java
@@ -1,16 +1,45 @@
 package simpledb;
 
+import java.util.Collection;
+
 public abstract class Impute extends Operator {
 
     private static final long serialVersionUID = 1L;
 
     protected DbIterator child;
     protected TupleDesc td;
-    
-    public Impute(DbIterator child){
+	protected final Collection<String> dropFields;
+
+    public Impute(Collection<String> dropFields, DbIterator child){
     	this.child = child;
     	this.td = child.getTupleDesc();
+		if (validateDropFields(dropFields)){
+			this.dropFields = dropFields;
+		} else {
+			throw new RuntimeException("Could not validate dropFields.");
+		}
     }
+
+	/**
+	 * Validate that each field in dropFields corresponds to a fieldName in
+	 * child's TupleDesc. Disallows null (anonymous) fields.
+	 * @param dropFields
+	 */
+	private boolean validateDropFields(Collection<String> dropFields) {
+		for (String field : dropFields){
+			if (field == null){
+				return false;
+			}
+
+			try{
+				td.fieldNameToIndex(field);
+			} catch (Exception e){
+				return false;
+			}
+		}
+		
+		return true;
+	}
 
 	@Override
 	public DbIterator[] getChildren() {
