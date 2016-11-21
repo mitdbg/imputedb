66915bb281d955823f1bac9b03c8acf9fbe0c57c
Micah Smith <micahjsmith@gmail.com>
2016-11-21 09:47:52 -0500

    WIP: Weka regression tree skeleton

--- simpledb/src/java/simpledb/ImputeRegressionTree.java
+++ simpledb/src/java/simpledb/ImputeRegressionTree.java
@@ -1,10 +1,20 @@
 package simpledb;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
+
+import weka.core.Instances;
+import weka.classifiers.Classifier;
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.trees.J48;
 
 public class ImputeRegressionTree extends Impute {
 
 	private static final long serialVersionUID = 1L;
+	private static final long GENERATOR_SEED = 6832L;
+	private Random random;
+	private ArrayList<Tuple> buffer;
 
 	public ImputeRegressionTree(Collection<String> dropFields, DbIterator child) {
 		super(dropFields, child);
@@ -12,14 +22,24 @@ public class ImputeRegressionTree extends Impute {
 
 	@Override
 	public void rewind() throws DbException, TransactionAbortedException {
-		// TODO Auto-generated method stub
+		child.rewind();
 		
 	}
 
 	@Override
 	protected Tuple fetchNext() throws DbException, TransactionAbortedException {
-		// TODO Auto-generated method stub
+		Instances train = null;
+		Classifier cls = new J48();
+		try {
+			cls.buildClassifier(train);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Evaluation eval;
+
 		return null;
+		
 	}
 
 }
