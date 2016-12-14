98b7ae3bdf1c37ae988f8e4705a7b6b78893f03b
Micah Smith <micahjsmith@gmail.com>
2016-12-14 03:47:31 -0500

    Generate example

---
+++ simpledb/test/example/dirty3.jl
@@ -0,0 +1,34 @@
+using DataFrames
+
+version = 3
+othp = 0.00
+telp = 0.15
+stp = 0.00
+infile = "acs.txt"
+outfile = "acs_dirty$version.txt"
+f = open(outfile, "w")
+
+df = readtable(infile, separator=',', header=false)
+
+iST = 1
+iTEL = 14
+d = ncol(df) # 37
+n = nrow(df) # 671153
+
+# fill in p
+p = othp * ones(d)
+p[iTEL] = telp
+p[iST] = stp
+
+for i in 1:n
+    row = Array{Any}(df[i,:]) # sketchy to allow us to print ""
+    for j in 1:d
+        if rand() < p[j]
+            row[j] = ""
+        end
+    end
+
+    println(f, join(row, ","))
+end
+
+close(f)
